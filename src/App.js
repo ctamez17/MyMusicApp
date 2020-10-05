089e9a14d98772f55e0cffd25acc6f72f05d7c3e
Meredith Davis <meredithd23@yahoo.com>
2020-10-05 17:15:56 -0500

    slider not working

--- src/App.js
+++ src/App.js
@@ -1,57 +1,63 @@
-import React, {Component} from 'react';
-import ButtonAppBar from './Components/ButtonAppBar'
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dashboard from './Components/Dashboard'
-import './App.css';
-
-
+import React, { Component } from "react";
+import ButtonAppBar from "./Components/ButtonAppBar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dashboard from "./Components/Dashboard";
+// import Test from "./Components/Dashboard";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       loggedIn: false,
-    }
+    };
   }
-  
+
   handleClick = () => {
     this.setState({
-        loggedIn: !this.state.loggedIn
-  
-    })
-    this.handleClick.bind(this)
-};
+      loggedIn: !this.state.loggedIn,
+    });
+    this.handleClick.bind(this);
+  };
 
   render() {
-    return this.state.loggedIn ? 
-        (<div>
-          <ButtonAppBar />
-          <Dashboard /> 
-        </div>)
-          : (
-        <div>
+    return this.state.loggedIn ? (
+      <div>
+        <ButtonAppBar />
+        <Dashboard />
+      </div>
+    ) : (
+      <div>
         <ButtonAppBar />
         <div className="login">
-        <TextField className="username"
-          id="username"
-          placeholder="username"
-          margin="normal"
-          variant="outlined">
-        </TextField>
-        <TextField className="password"
-          id="password"
-          placeholder="password"
-          margin="normal"
-          variant="outlined" color="white"></TextField> 
-         <Button onClick={this.handleClick} variant="contained" color="secondary">Login</Button>
-         </div>
-         </div>
-          )
-          
-  
-}
+          <TextField
+            className="username"
+            id="username"
+            placeholder="username"
+            margin="normal"
+            variant="outlined"
+          ></TextField>
+          <TextField
+            className="password"
+            id="password"
+            placeholder="password"
+            margin="normal"
+            variant="outlined"
+            color="white"
+          ></TextField>
+          <Button
+            onClick={this.handleClick}
+            variant="contained"
+            color="secondary"
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,27 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-// import Grid from '@material-ui/core/Grid'
+import InputSlider from "./Slider";
+import Switch from "./Switch";
 
-const Dashboard = () => {
-  return (
-    <div classname="music_cards">
-      <Card>
-        <CardMedia style={{ height: 0, paddingTop: "56.25%" }} />
-        <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
-            <Typography component="p">Here is a music card</Typography>
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary" target="_blank"></Button>
-        </CardActions>
-      </Card>
-    </div>
-  );
-};
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+const classes = useStyles();
+
+class Dashboard extends Component {
+  state = {
+    slider: false,
+  };
+
+  // handleKids = (x) => {
+  //   const k = Object.keys(x)[0];
+  //   const newState = {};
+  //   newState[k] = x[k];
+  //   this.setState(newState);
+  // };
+
+  render() {
+    return (
+      <div>
+        <h1 classname="welcome">Welcome User!</h1>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <InputSlider />
+            <Switch />
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
+
 export default Dashboard;
